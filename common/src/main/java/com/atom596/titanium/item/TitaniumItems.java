9910bb66f7128979169f90eb278b7113b832734b
Autumn <adamhoffensetz@gmail.com>
2025-02-27 14:34:27 -0500

    Fix double use bug on Testing Wand

--- common/src/main/java/com/atom596/titanium/item/TitaniumItems.java
+++ common/src/main/java/com/atom596/titanium/item/TitaniumItems.java
@@ -2,6 +2,7 @@ package com.atom596.titanium.item;
 
 import com.atom596.titanium.Titanium;
 import com.atom596.titanium.block.TitaniumBlocks;
+import com.atom596.titanium.item.testingwand.TestingWandItem;
 import com.atom596.titanium.regutils.RegistrationProvider;
 import com.atom596.titanium.regutils.RegistryObject;
 import net.minecraft.core.registries.BuiltInRegistries;
