323c6e4af3069fd52b48ce84e3340ef065dfb5db
Autumn <adamhoffensetz@gmail.com>
2025-05-14 12:16:39 -0400

    Continue work on dynamic tools

--- common/src/main/java/com/atom596/titanium/item/dyntools/DynamicSwordItem.java
+++ common/src/main/java/com/atom596/titanium/item/dyntools/DynamicSwordItem.java
@@ -1,6 +1,8 @@
 package com.atom596.titanium.item.dyntools;
 
 import net.minecraft.ChatFormatting;
+import net.minecraft.client.Minecraft;
+import net.minecraft.client.gui.screens.Screen;
 import net.minecraft.network.chat.CommonComponents;
 import net.minecraft.network.chat.Component;
 import net.minecraft.world.item.*;
@@ -12,11 +14,38 @@ public class DynamicSwordItem extends SwordItem {
         super(tier, properties);
     }
 
+    private Component getBaseName() {
+        return Component.translatable("item.titanium.dynamic_gear.material.iron")
+                .withStyle(ChatFormatting.WHITE);
+    }
+
+    private void addDetailedComponents(List<Component> components) {
+        components.add(Component.translatable("item.titanium.dynamic_gear.perks_from", getBaseName())
+                .withStyle(ChatFormatting.GRAY));
+
+        if (false) { // The base provides any benefits
+
+        } else {
+            components.add(CommonComponents.SPACE.copy()
+                    .append(Component.translatable("item.titanium.dynamic_gear.no_change"))
+                    .withStyle(ChatFormatting.DARK_GREEN));
+        }
+    }
+
     @Override
     public void appendHoverText(ItemStack itemStack, TooltipContext context, List<Component> components, TooltipFlag flag) {
-        components.add(Component.translatable("item.titanium.dynamic_gear.base")
-                .append(CommonComponents.SPACE)
-                .append(Component.translatable("item.titanium.dynamic_gear.material.iron"))
+        components.add(Component.translatable("item.titanium.dynamic_gear.base", getBaseName())
                 .withStyle(ChatFormatting.GRAY));
+        components.add(Component.empty());
+
+        if (!Screen.hasShiftDown()) {
+            components.add(Component.translatable("item.titanium.dynamic_gear.hold_shift",
+                            Minecraft.getInstance().options.keyShift
+                                    .getTranslatedKeyMessage().copy()
+                                    .withStyle(ChatFormatting.GRAY))
+                    .withStyle(ChatFormatting.DARK_GRAY));
+        } else {
+            addDetailedComponents(components);
+        }
     }
 }
